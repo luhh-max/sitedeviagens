e89f9194c66cee76db0734fd23ca53036a130085
luhh-max <luizasilvapinto053@gmail.com>
2024-09-11 11:29:28 -0300

    Add files via upload

---
+++ projeto/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado por entrar em contato! Responderei o mais breve possível.');
+});
